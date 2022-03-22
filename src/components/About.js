d082c1dc68103163917040ed7a1cdf815625d79e
hakareem <harithakareem72.com>
2022-03-22 18:01:56 +0000

    project desc

--- src/components/About.js
+++ src/components/About.js
@@ -10,12 +10,14 @@ const About = () => {
         </Typography>
         <Typography variant='h5'>
           Built using MERN STACK - MongoDB | Express.js | React.js | Node.js
-          <br></br>
-          <h5>
-            Learned Experience:
-            <br></br>
-            React Hooks
-          </h5>
+        </Typography>
+        <Typography>
+          <p>
+            I created this full stack responsive web app where I'm able to add
+            books to my collection. This was a fantastic learning experience, I
+            was able to go in depth and learn about both front-end and back-end
+            libraries.
+          </p>
         </Typography>
       </Box>
     </div>
